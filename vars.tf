533ba5c616781d8fddef9efcdb2d6eac2b6e9631
purna <purnaraomba@gmail.com>
2024-03-28 19:34:41 +0530

    Roboshop Update

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "env" {}
+variable "tags" {}
+variable "engine" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "engine_version" {}
+variable "subnet_ids" {}
+variable "no_of_instances" {}
+variable "instance_class" {}
+
+variable "storage_encrypted" {
+  default = true
+}
