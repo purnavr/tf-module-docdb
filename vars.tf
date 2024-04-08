666ace8380c6fe98231e427fab46bbae3225a135
purna <purnaraomba@gmail.com>
2024-04-08 18:00:28 +0530

    Roboshop Update

--- vars.tf
+++ vars.tf
@@ -5,7 +5,7 @@ variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
 variable "engine_version" {}
-variable "subnet_ids" {}
+#variable "subnet_ids" {}
 variable "no_of_instances" {}
 variable "instance_class" {}
 
@@ -13,5 +13,5 @@ variable "storage_encrypted" {
   default = true
 }
 
-variable "allow_subnets" {}
-variable "vpc_id" {}
+#variable "allow_subnets" {}
+#variable "vpc_id" {}
