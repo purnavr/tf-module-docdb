230a8720e7acae63fbe035a09ed9fa18229b4b78
purna <purnaraomba@gmail.com>
2024-04-09 23:47:39 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -20,68 +20,70 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   instance_class     = var.instance_class
 }
 
-#resource "aws_security_group" "main" {
-#  name        = "docdb-${var.env}"
-#  description = "docdb-${var.env}"
-#  vpc_id      = var.vpc_id
-#
-#  ingress {
-#    description = "DOCDB"
-#    from_port   = 27017
-#    to_port     = 27017
-#    protocol    = "tcp"
-#    cidr_blocks = var.allow_subnets
-#  }
-#
-#  tags = merge(
-#    var.tags,
-#    { Name = "docdb-${var.env}" }
-#  )
-#}
-#
-#
-##resource "aws_vpc_security_group_ingress_rule" "ingress2" {
-##  security_group_id = aws_security_group.main.id
-##  cidr_ipv4         = var.allow_app_to
-##  from_port         = var.port
-##  ip_protocol       = "tcp"
-##  to_port           = var.port
-##  description = "APP"
-##}
-#
-#resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_security_group" "main" {
+  name        = "docdb-${var.env}"
+  description = "docdb-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "DOCDB"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "docdb-${var.env}" }
+  )
+}
+
+
+#resource "aws_vpc_security_group_ingress_rule" "ingress2" {
 #  security_group_id = aws_security_group.main.id
-#  cidr_ipv4         = "0.0.0.0/0"
-#  ip_protocol       = "-1" # semantically equivalent to all ports
+#  cidr_ipv4         = var.allow_app_to
+#  from_port         = var.port
+#  ip_protocol       = "tcp"
+#  to_port           = var.port
+#  description = "APP"
 #}
-#
-#resource "aws_docdb_subnet_group" "main" {
-#  name       = "${var.env}-docdb"
-#  subnet_ids = var.subnet_ids
-#
-#  tags = merge(
-#    var.tags,
-#    { Name = "${var.env}-subnet-group" }
-#  )
-#}
-#
-#resource "aws_ssm_parameter" "docdb_url" {
-#  name = "${var.env}.docdb.url.catalogue"
-#  type = "String"
-#  value = var.parameters[count.index].value
-#}
-#
-#resource "aws_ssm_parameter" "docdb_url_user" {
-#  name = "${var.env}.docdb.url.user"
-#  type = "String"
-#  value = var.parameters[count.index].value
-#}
-#
-#resource "aws_ssm_parameter" "docdb_endpoint" {
-#  name = "${var.env}.docdb.endpoint"
-#  type = "String"
-#  value = aws_docdb_cluster.main.endpoint
-#}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_docdb_subnet_group" "main" {
+  name       = "${var.env}-docdb"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-subnet-group" }
+  )
+}
+
+
+resource "aws_ssm_parameter" "docdb_url" {
+  name = "${var.env}.docdb.url.catalogue"
+  type = "String"
+  #value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@dev-docdb.cluster-clarhbmazh1y.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+  value = value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@${aws_docdb_cluster.docdb.endpoint}:27017/catalogue?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name = "${var.env}.docdb.url.user"
+  type = "String"
+  value = value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@${aws_docdb_cluster.docdb.endpoint}:27017/users?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_endpoint" {
+  name = "${var.env}.docdb.endpoint"
+  type = "String"
+  value = aws_docdb_cluster.main.endpoint
+}
 
 
 
