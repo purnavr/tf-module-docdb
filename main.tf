08b3becf9bf32691d44a1acee7d0ec48c1fbbeeb
purna <purnaraomba@gmail.com>
2024-07-01 18:46:29 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "main" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.main.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1"
 }
 
 resource "aws_docdb_subnet_group" "main" {
